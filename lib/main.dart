bdcaf190f487429ef5d80ef8bf2b8e5b554dd1a5
Adam Cutmore <adam@swiftkey.com>
2018-04-18 21:29:36 +0100

    basic homepage

--- lib/main.dart
+++ lib/main.dart
@@ -3,124 +3,114 @@ import 'package:english_words/english_words.dart';
 
 void main() => runApp(new MyApp());
 
+const minPlayers = 5;
+const maxPlayers = 10;
+
 class MyApp extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
+
     return new MaterialApp(
-      title: 'Welcome to Flutter',
+      title: 'Heist',
       theme: new ThemeData(
         primaryColor: Colors.deepOrange,
       ),
-      home: new RandomWords()
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text("Heist"),
+        ),
+        body: new HomePage()
+      )
     );
   }
 }
 
-class RandomWords extends StatefulWidget {
+class HomePage extends StatefulWidget {
   @override
-  createState() => new RandomWordsState();
+  State<StatefulWidget> createState() {
+    return new HomePageState();
+  }
 }
 
-class RandomWordsState extends State<RandomWords> {
+class HomePageState extends State<HomePage> {
 
-  final _suggestions = <WordPair>[];
-  final _saved = new Set<WordPair>();
-  final _biggerFont = const TextStyle(fontSize: 18.0);
-
-  void _pushSaved() {
-    Navigator.of(context).push(
-      new MaterialPageRoute(
-        builder: (context) {
-          final tiles = _saved.map(
-                  (pair) {
-              return new ListTile(
-                title: new Text(
-                  pair.asPascalCase,
-                  style: _biggerFont,
-                ),
-              );
-            },
-          );
-          final divided = ListTile
-              .divideTiles(
-            context: context,
-            tiles: tiles,
-          ).toList();
-
-          return new Scaffold(
-            appBar: new AppBar(
-              title: new Text('Saved Suggestions'),
-            ),
-            body: new ListView(children: divided),
-          );
-        },
-      ),
-    );
-  }
+  var _numPlayers = minPlayers;
 
   @override
   Widget build(BuildContext context) {
-    return new Scaffold (
-      appBar: new AppBar(
-        title: new Text('Startup Name Generator'),
-        actions: <Widget>[
-          new IconButton(icon: new Icon(Icons.list), onPressed: _pushSaved),
-        ],
+
+    Column buildArrowColumn(IconData icon, Function onPressed) {
+      Color color = Theme.of(context).primaryColor;
+      return new Column(
+          children: [
+            new IconButton(
+              iconSize: 64.0,
+              onPressed: onPressed,
+              icon: new Icon(icon, color: color)
+            )
+          ],
+      );
+    }
+
+    Widget numPlayersText = new Container(
+      padding: const EdgeInsets.all(32.0),
+      child: new Text(
+        _numPlayers.toString(),
+        style: new TextStyle(
+          fontSize: 32.0,
+        ),
       ),
-      body: _buildSuggestions(),
     );
-  }
 
-  Widget _buildSuggestions() {
-    return new ListView.builder(
-        padding: const EdgeInsets.all(16.0),
-        // The itemBuilder callback is called once per suggested word pairing,
-        // and places each suggestion into a ListTile row.
-        // For even rows, the function adds a ListTile row for the word pairing.
-        // For odd rows, the function adds a Divider widget to visually
-        // separate the entries. Note that the divider may be difficult
-        // to see on smaller devices.
-        itemBuilder: (context, i) {
-          // Add a one-pixel-high divider widget before each row in theListView.
-          if (i.isOdd) return new Divider();
-
-          // The syntax "i ~/ 2" divides i by 2 and returns an integer result.
-          // For example: 1, 2, 3, 4, 5 becomes 0, 1, 1, 2, 2.
-          // This calculates the actual number of word pairings in the ListView,
-          // minus the divider widgets.
-          final index = i ~/ 2;
-          // If you've reached the end of the available word pairings...
-          if (index >= _suggestions.length) {
-            // ...then generate 10 more and add them to the suggestions list.
-            _suggestions.addAll(generateWordPairs().take(10));
-          }
-          return _buildRow(_suggestions[index]);
-        }
+    Widget numPlayersTitle = new Container(
+      padding: const EdgeInsets.all(32.0),
+      child: const Text(
+        "Choose number of players:",
+        style: const TextStyle(fontSize: 16.0),
+      ),
     );
-  }
 
-  Widget _buildRow(WordPair pair) {
+    Widget numPlayers = new Column(
+      children: [
+        numPlayersTitle,
+        new Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            buildArrowColumn(Icons.arrow_back, () => setState(() {
+              if (_numPlayers > minPlayers) _numPlayers--;
+            })),
+            numPlayersText,
+            buildArrowColumn(Icons.arrow_forward, () => setState(() {
+              if (_numPlayers < maxPlayers) _numPlayers++;
+            }))
+          ],
+        )
+      ],
+    );
 
-    final alreadySaved = _saved.contains(pair);
+    void createRoom() {
+      print('CREATE ROOM');
+    }
 
-    return new ListTile(
-      title: new Text(
-        pair.asPascalCase,
-        style: _biggerFont,
+    Widget createRoomButton = new Container(
+      padding: const EdgeInsets.all(32.0),
+      child: new RaisedButton(
+        child: const Text(
+          'CREATE ROOM',
+          style: const TextStyle(color: Colors.white, fontSize: 16.0)
+        ),
+        onPressed: createRoom,
+        color: Theme.of(context).primaryColor,
       ),
-      trailing: new Icon(
-        alreadySaved ? Icons.favorite : Icons.favorite_border,
-        color: alreadySaved ? Colors.red : null,
-      ),
-      onTap: () {
-        setState(() {
-          if (alreadySaved) {
-            _saved.remove(pair);
-          } else {
-            _saved.add(pair);
-          }
-        });
-      },
+    );
+
+    return new Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        numPlayers,
+        createRoomButton
+      ],
     );
   }
 }

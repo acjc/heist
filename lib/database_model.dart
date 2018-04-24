204c2eea8228b1936cf1580fcc3a3958041d8b48
Adam Cutmore <adam@swiftkey.com>
2018-04-24 22:11:18 +0100

    show rooms

---
+++ lib/database_model.dart
@@ -0,0 +1,27 @@
+
+class Room {
+
+  final String id;
+  final String code;
+  final DateTime createdAt;
+  final String appVersion;
+  final bool completed;
+  final DateTime completedAt;
+  final int numPlayers;
+  final Set<String> roles = new Set();
+
+  Room.fromJson(Map<String, dynamic> json)
+      : id = json['id'],
+        code = json['code'],
+        createdAt = json['createdAt'],
+        appVersion = json['appVersion'],
+        completed = json['completed'],
+        completedAt = json['completedAt'],
+        numPlayers = json['numPlayers'] {
+    json['roles'].forEach((role, b) {
+      if (b) {
+        this.roles.add(role);
+      }
+    });
+  }
+}

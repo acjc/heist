9532fc6450cd88ebcd6eafbd1a4c9f89b104f508
Adam Cutmore <1249740+acjc@users.noreply.github.com>
2018-07-19 22:54:56 +0100

    spurious F

--- lib/main.dart
+++ lib/main.dart
@@ -4,62 +4,62 @@ import 'dart:async';
 import 'dart:math';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/foundation.dart';
 import 'package:collection/collection.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_redux/flutter_redux.dart';
+import 'package:flutter_redux_dev_tools/flutter_redux_dev_tools.dart';
 import 'package:package_info/package_info.dart';
 import 'package:redux/redux.dart';
-import 'package:redux_logging/redux_logging.dart';
-import 'package:flutter_redux_dev_tools/flutter_redux_dev_tools.dart';
 import 'package:redux_dev_tools/redux_dev_tools.dart';
+import 'package:redux_logging/redux_logging.dart';
 import 'package:reselect/reselect.dart';
-import 'package:uuid/uuid.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:uuid/uuid.dart';
 
 part 'db/database.dart';
 part 'db/database_model.dart';
-part 'widget/game.dart';
-part 'widget/home_page.dart';
-part 'widget/player_info.dart';
-part 'widget/game_history.dart';
-part 'widget/bidding.dart';
-part 'widget/gifting.dart';
-part 'widget/round_end.dart';
-part 'widget/common.dart';
-part 'widget/selection_board.dart';
-part 'widget/team_picker.dart';
-part 'widget/decision.dart';
-part 'widget/heist_end.dart';
-part 'widget/endgame.dart';
+part 'heist_definitions.dart';
+part 'middleware/bidding_middleware.dart';
+part 'middleware/game_middleware.dart';
+part 'middleware/gifting_middleware.dart';
+part 'middleware/heist_middleware.dart';
 part 'middleware/middleware.dart';
 part 'middleware/room_middleware.dart';
-part 'middleware/game_middleware.dart';
-part 'middleware/bidding_middleware.dart';
-part 'middleware/gifting_middleware.dart';
 part 'middleware/round_end_middleware.dart';
 part 'middleware/team_picker_middleware.dart';
-part 'middleware/heist_middleware.dart';
+part 'reducers/bid_amount_reducers.dart';
+part 'reducers/gift_amount_reducers.dart';
 part 'reducers/heist_reducers.dart';
 part 'reducers/player_reducers.dart';
 part 'reducers/reducers.dart';
+part 'reducers/request_reducers.dart';
 part 'reducers/room_reducers.dart';
 part 'reducers/round_reducers.dart';
 part 'reducers/subscription_reducers.dart';
-part 'reducers/request_reducers.dart';
-part 'reducers/bid_amount_reducers.dart';
-part 'reducers/gift_amount_reducers.dart';
-part 'selectors/selectors.dart';
-part 'selectors/setup_selectors.dart';
-part 'selectors/player_selectors.dart';
-part 'selectors/team_picker_selectors.dart';
+part 'role.dart';
 part 'selectors/bidding_selectors.dart';
 part 'selectors/gifting_selectors.dart';
 part 'selectors/heist_selectors.dart';
+part 'selectors/player_selectors.dart';
+part 'selectors/selectors.dart';
+part 'selectors/setup_selectors.dart';
+part 'selectors/team_picker_selectors.dart';
 part 'state.dart';
-part 'role.dart';
-part 'heist_definitions.dart';
+part 'widget/bidding.dart';
+part 'widget/common.dart';
+part 'widget/decision.dart';
+part 'widget/endgame.dart';
+part 'widget/game.dart';
+part 'widget/game_history.dart';
+part 'widget/gifting.dart';
+part 'widget/heist_end.dart';
+part 'widget/home_page.dart';
+part 'widget/player_info.dart';
+part 'widget/round_end.dart';
+part 'widget/selection_board.dart';
+part 'widget/team_picker.dart';
 
 void main() => runApp(new MyApp(Firestore.instance));
 
@@ -86,7 +86,7 @@ Store<GameModel> createStore(FirestoreDb db, [int numPlayers]) {
   if (isDebugMode()) {
     return new DevToolsStore<GameModel>(
       gameModelReducer,
-      initialState: new GameModel.initial(db, numPlayers ?? F2),
+      initialState: new GameModel.initial(db, numPlayers ?? 2),
       middleware: createMiddleware(),
       distinct: true,
     );
@@ -121,9 +121,8 @@ class MyApp extends StatelessWidget {
             appBar: new AppBar(
               title: new Text("Heist"),
             ),
-            endDrawer: isDebugMode()
-                ? new Drawer(child: new ReduxDevTools<GameModel>(store))
-                : null,
+            endDrawer:
+                isDebugMode() ? new Drawer(child: new ReduxDevTools<GameModel>(store)) : null,
             body: new HomePage(),
           )),
     );

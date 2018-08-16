93f21fd19f1ada2ec32d759a9e416e9ea2f25682
Adam Cutmore <1249740+acjc@users.noreply.github.com>
2018-08-16 23:12:06 +0100

    give up on version name

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-dpl --provider=gcs --skip_cleanup=true --access-key-id=$GCS_KEY --secret-access-key=$GCS_SECRET --bucket=$BUCKET --local-dir=./build/app/outputs/apk/release --upload-dir=releases
+dpl --provider=gcs --skip_cleanup=true --access-key-id=$GCS_KEY --secret-access-key=$GCS_SECRET --bucket=$BUCKET --local-dir=./build/app/outputs/apk/release --upload-dir=releases/${TRAVIS_COMMIT}_${TRAVIS_BUILD_NUMBER}

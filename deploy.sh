feacca9e594ea912b4cea06dceaf0eedcd7c64d1
Adam Cutmore <1249740+acjc@users.noreply.github.com>
2018-08-16 23:40:37 +0100

    extract version name

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-dpl --provider=gcs --skip_cleanup=true --access-key-id=$GCS_KEY --secret-access-key=$GCS_SECRET --bucket=$BUCKET --local-dir=./build/app/outputs/apk/release --upload-dir=releases/${TRAVIS_COMMIT}_${TRAVIS_BUILD_NUMBER}
+version=$(jq -r '.[] | .apkInfo | .versionName' < build/app/outputs/apk/release/output.json)
+
+dpl --provider=gcs --skip_cleanup=true --access-key-id=$GCS_KEY --secret-access-key=$GCS_SECRET --bucket=$BUCKET --local-dir=./build/app/outputs/apk/release --upload-dir=releases/${version}/${TRAVIS_COMMIT}/${TRAVIS_BUILD_NUMBER}
